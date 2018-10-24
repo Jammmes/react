4c0638252e394bf2fc4c607c216fe759de4211a3
Eugen <glazov.it@gmail.com>
2018-10-24 16:26:37 +0400

    lesson2 - start

--- app/src/jsx/index.jsx
+++ app/src/jsx/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
 import DevInfoComponent from '../components/DevInfoComponent.jsx';
+import devList from '../modules/devlist.js';
+
+
 
 
 class App extends React.Component{
+
     render(){
         return( 
         <div>
-        <p> Hello React !!! </p>
         <DevInfoComponent />
+        <p> Hello React !!! </p>
+                {devList.forEach((item) => <div><p>{item.name}</p></div>
+            )}
         </div>
         )
     }
---
+++ app/src/modules/devlist.js
@@ -0,0 +1,27 @@
+const devList = [{
+        name: "Василий",
+        surname: "Василев",
+        lastname: "Васильевич",
+        img: "bg.jpg"
+    },
+    {
+        name: "Петр",
+        surname: "Петров",
+        lastname: "Петрович",
+        img: "gf.jpg"
+    },
+    {
+        name: "Сергей",
+        surname: "Сергеев",
+        lastname: "Сергеевич",
+        img: "im.jpg"
+    },
+    {
+        name: "Дмитрий",
+        surname: "Дмитриев",
+        lastname: "Дмитриевич",
+        img: "jc.jpg"
+    }
+]
+
+export default devList;
