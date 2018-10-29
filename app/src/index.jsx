df01d37814e17c106db147186a4c6b3d36434dee
jammmes <glazov.it@gmail.com>
2018-10-29 09:33:48 +0400

    start lesson3

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
-import DevListComponent from '../components/DevListComponent.jsx';
-import devList from '../modules/devlist.js';
+import DevListComponent from './components/DevListComponent.jsx';
+import devList from './modules/devlist.js';
 
 class App extends React.Component{
     render(){
--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,23 @@ const BUILD_DIR = path.resolve(__dirname, 'app/dist');
 const APP_DIR = path.resolve(__dirname, 'app/src');
 
 var config = {
-    entry: APP_DIR + '/app/index.jsx',
-    output: {
-        path: BUILD_DIR,
-        filename: 'build.js'
-    },
-    module: {
-        rules: [{
-            test: /\.jsx?/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env'],
-                    plugins: ['@babel/plugin-proposal-object-rest-spread']
-                }
-            }
-        }]
-    }
+  entry: APP_DIR + '/index.jsx',
+  output: {
+    path: BUILD_DIR,
+    filename: 'build.js'
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-object-rest-spread']
+        }
+      }
+    }]
+  }
 };
 
 module.exports = config;
